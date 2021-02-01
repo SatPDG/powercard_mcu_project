814564c76fbd201472876a9ddde74f79e50147b3
SatPDG <leoclouet@gmail.com>
2021-02-01 12:14:16 -0500

    Ajout du système de paramètre

---
+++ PowerCardMCU/parametersSystem/parameterSystem.h
@@ -0,0 +1,19 @@
+/*
+ * parameterSystem.h
+ *
+ *  Created on: Feb 1, 2021
+ *      Author: RMDS
+ */
+
+#ifndef PARAMETERSYSTEM_H_
+#define PARAMETERSYSTEM_H_
+
+void ParameterSystem_Init();
+
+void ParameterSystem_Load();
+
+void ParameterSystem_ResetToDefault();
+
+void ParameterSystem_CommitToFlash();
+
+#endif /* PARAMETERSYSTEM_H_ */
